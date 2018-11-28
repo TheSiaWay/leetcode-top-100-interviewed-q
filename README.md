14a30b83519e2a527ae75fa90269e7b7d210a3ea
TheSiaWay <thesiaway@gmail.com>
2018-11-28 08:34:34 -0800

    fizz buzz

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # leetcode-top-100-interviewed-q
 [x] [reverse string](./reverseString.js)
+[x] [fizz buzz](./fizzBuzz.js)
---
+++ fizzBuzz.js
@@ -0,0 +1,77 @@
+/*Write a program that outputs the string representation of numbers from 1 to n.
+
+But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+
+Example:
+
+n = 15,
+
+Return:
+[
+    "1",
+    "2",
+    "Fizz",
+    "4",
+    "Buzz",
+    "Fizz",
+    "7",
+    "8",
+    "Fizz",
+    "Buzz",
+    "11",
+    "Fizz",
+    "13",
+    "14",
+    "FizzBuzz"
+] */
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+const fizzBuzz = (n) =>  {
+  const result = [];
+  for (let i = 1; i <= n; i++) {
+    result.push(fizzBuzzHelper(i));
+  }
+  return result;
+};
+var fizzBuzzHelper = function (n) {
+  if (n % 15 === 0) {
+    return 'FizzBuzz';
+  } else if (n % 3 === 0) {
+    return 'Fizz';
+  } else if (n % 5 === 0) {
+    return 'Buzz';
+  } else {
+    return `${n}`;
+  }
+}
+
+/**
+ * Approach 2 where instead of having explicitly stating the conditions, mapping the conditions in fizz buzz map
+ * and then check each number against the map
+ * @param {number} n
+ */
+const fizzBuzz2 = (n) => {
+  const map = {
+    3: 'Fizz',
+    5: 'Buzz'
+  }
+  const result = [];
+  for (let i = 1; i <= n; i++) {
+    let str = '';
+    for (let key in map) {
+      if (i % parseInt(key) === 0) {
+        str += map[key];
+      }
+    }
+    if (str.length === 0) {
+      str = `${i}`;
+    }
+    result.push(str);
+  }
+  return result;
+}
+
+console.log(fizzBuzz(15));
+console.log(fizzBuzz2(15));
