615863f37439206fa41197e4f27701986a41a0c8
TheSiaWay <thesiaway@gmail.com>
2018-11-28 08:15:27 -0800

    reverse string

---
+++ README.md
@@ -0,0 +1,2 @@
+# leetcode-top-100-interviewed-q
+[x] [reverse string](./reverseString.js)
---
+++ reverseString.js
@@ -0,0 +1,27 @@
+/*Write a function that takes a string as input and returns the string reversed.
+
+Example 1:
+
+Input: "hello"
+Output: "olleh"
+Example 2:
+
+Input: "A man, a plan, a canal: Panama"
+Output: "amanaP :lanac a ,nalp a ,nam A" */
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseString = function (s) {
+  s = s.split('');
+  for (let i = 0, j = s.length - 1; i < j; i++, j--) {
+    let temp = s[i];
+    s[i] = s[j];
+    s[j] = temp;
+  }
+  return s.join('');
+};
+
+console.log(reverseString('hello'));
+console.log(reverseString('A man, a plan, a canal: Panama'));
